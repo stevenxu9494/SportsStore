6e3cd9adec37e95759cd758caffaa4673598cb59
Steven Xu <stevenxu9494@gmail.com>
2020-06-02 16:44:36 +1000

    Change the url link to "http://<myserver>:<port>/Chess/Page1", and prevent showing non-relevant category

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -22,11 +22,12 @@ namespace SportsStore.WebUI.Controllers
         // {
         //     return View(repository.Products);
         // }
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * PageSize)
                     .Take(PageSize),
@@ -35,7 +36,8 @@ namespace SportsStore.WebUI.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
