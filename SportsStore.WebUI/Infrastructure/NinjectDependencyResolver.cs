b6ffbab75fd41d5be6ac11a36620ff5e3fb527c0
Steven Xu <stevenxu9494@gmail.com>
2020-06-09 10:24:54 +1000

    restore

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Configuration;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
@@ -30,6 +31,15 @@ namespace SportsStore.WebUI.Infrastructure
         {
             // put bindings here
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+                .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
