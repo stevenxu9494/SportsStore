ba732dbc7fe3c3c668be1bdeb242bb49378164c2
Steven Xu <stevenxu9494@gmail.com>
2020-06-09 17:12:49 +1000

    Adding Admin Functionalities (CRUD)

--- SportsStore.Domain/Abstract/IProductsRepository.cs
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -9,5 +9,7 @@ namespace SportsStore.Domain.Abstract
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+        void SaveProduct(Product product);
+        Product DeleteProduct(int productID);
     }
 }
